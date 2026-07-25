e6cf7bdf08d50f6521004a500d521bb0a9b003e6
GauravJ02 <99jaingaurav@gmail.com>
2026-07-25 13:04:46 +0530

    Database

---
+++ D12/D12_P1.py
@@ -0,0 +1,20 @@
+Students
+| StudentID | Name  | Branch |
+| --------- | ----- | ------ |
+| 101       | Rahul | CSE    |
+| 102       | Priya | ECE    |
+| 103       | Ankit | CSE    |
+
+courses
+| CourseID | Course |
+| -------- | ------ |
+| 1        | Python |
+| 2        | DBMS   |
+
+marks
+| StudentID | CourseID | Marks |
+| --------- | -------- | ----: |
+| 101       | 1        |    90 |
+| 101       | 2        |    80 |
+| 102       | 1        |    75 |
+| 103       | 2        |    95 |
